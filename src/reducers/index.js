473f3d4da8ce320975075a6ed2e4b65fe3c99cfd
mergin <merginvalhala@gmail.com>
2018-01-15 09:11:43 -0400

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+// root reducer
+
+import { combineReducers } from 'redux';
+import images from './imageReducer';
+import videos from './videoReducer';
+
+// Combines all reducers to a single reducer function
+const rootReducer = combineReducers({
+    images,
+    videos
+});
+
+export default rootReducer;
